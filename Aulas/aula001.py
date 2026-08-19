616d3a528fb9d19b29f380298114a32cb00f5d65
Joaquim <joaquimlima0612@gmail.com>
2026-08-19 16:47:17 -0300

    Término da parte teórica

--- Aulas/aula001.py
+++ Aulas/aula001.py
@@ -14,3 +14,9 @@
     # Naturalidade = Mais fácil de entender. Maior atenção às funcionalidades do que aos detalhes de implementação (ex.: quando você for comprar um carro você precisa saber exatamente o processo de aceleração? Não, pois você é o usuário)
 
 # ============= DEFINIÇÕES =============
+# -> Classe: Formato a ser seguido sempre que for fazer um objeto do mesmo tipo ou com as mesmas características ou com os mesmos comportamentos (ex.: a forma de um biscoito, um chassi de um carro, etc.)
+    # -> Diagrama de Classes UML: nome de classe, características que ela vai ter (atributos) e coisas que podem fazer (métodos)
+# -> Instância: Seguir o padrão que foi definido na classe para poder criar um objeto (ex.: a forma do biscoito pressionando a massa, estou instanciando a forma)
+# -> Objeto: Coisa material ou abstrata que é feita a partir de um modelo e pode ser descrita por meio das suas características, comportamentos e estado atual (ex.: o resultado do biscoito após pressionar a forma na massa pura)
+# -> Estado: Especificação do objeto (ex.: um biscoito a 55°C, mordido, de pistache, etc.)
+# -> Objetos Abstratos: Uma consulta marcada no médico, um processo de venda, um compromisso ou reunião, uma aula na faculdade, uma transação bancária, uma reserva de voo, um erro do sistema...
